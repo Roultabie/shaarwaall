bab966b31a38e51053f7ff103e493421064afb19
Daniel Douat <daniel.douat@gorgones.net>
2018-05-30 15:48:43 +0200

    remove tests.php from git index

--- tests.php
+++ tests.php
@@ -9,9 +9,13 @@ $flowDb = new flowDb();
 $sharers = $flowDb->getSharers();
 foreach($sharers as $sharer) {
     $flow = feedParser::loadFeed($sharer->uri);
-    $result[] = $flowDb->addElements($sharer->id, $flow);
+    //$result[] = $flowDb->addElements($sharer->id, $flow);
 }
+//$result = $flowDb->editSharer(1, ['title' => 'Daniel Links ovekill']);
+// $result = $flowDb->getSharerUpdatedFeed(3);
+// $flowDb->setSharerUpdatedFeed(3, '2018-05-28T21:08:35+02:00');
+// $result2 = $flowDb->getSharerUpdatedFeed(3);
+//$json  = json_encode($flow->entry);
 
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
+$array = flowDb::flowToArray($flow->entry, 'published', 'ASC', '2018-05-06 14:51:13', '2018-05-28 00:00:00');
+var_dump($array);
