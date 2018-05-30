fda458e0d1574ef534c941c6d4fee811f752540c
Daniel Douat <daniel.douat@gorgones.net>
2018-05-30 21:01:19 +0200

    rewrited addElements

--- libs/flowdb.php
+++ libs/flowdb.php
@@ -11,84 +11,83 @@ class flowDb
     {
         //$this->causes
     }
-    public function addElements($id, $obj) // array = links to add (array key[] = array key = fields, value = values)
+    public function addElements($id, $datas) // array = links to add (array key[] = array key = fields, value = values)
     {
-        if (is_object($obj)) {
-            if (count($obj) > 0) {
-                if (count($obj->entry) > 0) {
-                    $query = 'INSERT INTO flow (sharer, link_hash, link, title, content, tags, permalink, published, updated, first_share, id)
-                              VALUES (:sharer, :link_hash, :link, :title, :content, :tags, :permalink,  :published, :updated, :first_share, :id);';
-                    $stmt = dbConnexion::getInstance()->prepare($query);
-                    $stmt->bindParam(':sharer', $sharer, PDO::PARAM_INT);
-                    $stmt->bindParam(':link_hash', $link_hash, PDO::PARAM_STR);
-                    $stmt->bindParam(':link', $link, PDO::PARAM_STR);
-                    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-                    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
-                    $stmt->bindParam(':tags', $taglist, PDO::PARAM_STR);
-                    $stmt->bindParam(':permalink', $permalink, PDO::PARAM_STR);
-                    $stmt->bindParam(':published', $published, PDO::PARAM_STR);
-                    $stmt->bindParam(':updated', $updated, PDO::PARAM_STR);
-                    $stmt->bindParam(':first_share', $firstShare, PDO::PARAM_STR);
-                    $stmt->bindParam(':id', $smallHash, PDO::PARAM_STR);
+        if (is_array($datas)) {
+            $query = 'INSERT INTO flow (
+                sharer, link_hash, link, title, content,
+                tags, permalink, published, updated, first_share, id)
+                VALUES (:sharer, :link_hash, :link, :title, :content,
+                :tags, :permalink,  :published, :updated, :first_share, :id)
+                ON DUPLICATE KEY UPDATE link_hash = :link_hash, link = :link,
+                title = :title, content = :content, tags = :tags, published = :published,
+                updated = :updated, first_share = :first_share;';
+            $stmt = dbConnexion::getInstance()->prepare($query);
+            $stmt->bindParam(':sharer', $sharer, PDO::PARAM_INT);
+            $stmt->bindParam(':link_hash', $link_hash, PDO::PARAM_STR);
+            $stmt->bindParam(':link', $link, PDO::PARAM_STR);
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+            $stmt->bindParam(':tags', $taglist, PDO::PARAM_STR);
+            $stmt->bindParam(':permalink', $permalink, PDO::PARAM_STR);
+            $stmt->bindParam(':published', $published, PDO::PARAM_STR);
+            $stmt->bindParam(':updated', $updated, PDO::PARAM_STR);
+            $stmt->bindParam(':first_share', $firstShare, PDO::PARAM_STR);
+            $stmt->bindParam(':id', $footPrint, PDO::PARAM_STR);
 
-                    $pQuery = 'INSERT INTO flow_pending (sharer, updated, object, hash)
-                               VALUES (:pSharer, NOW(), :pObject, :pHash)
-                               ON DUPLICATE KEY UPDATE updated = NOW();';
-                    $pStmt = dbConnexion::getInstance()->prepare($pQuery);
-                    $pStmt->bindParam(':pSharer', $pSharer, PDO::PARAM_STR);
-                    $pStmt->bindParam(':pObject', $pObject, PDO::PARAM_STR);
-                    $pStmt->bindParam(':pHash', $pHash, PDO::PARAM_STR);
-                    $pending = false;
+            $pQuery = 'INSERT INTO flow_pending (sharer, updated, datas, id)
+                VALUES (:pSharer, NOW(), :pDatas, :pId)
+                ON DUPLICATE KEY UPDATE updated = NOW();';
+            $pStmt = dbConnexion::getInstance()->prepare($pQuery);
+            $pStmt->bindParam(':pSharer', $pSharer, PDO::PARAM_STR);
+            $pStmt->bindParam(':pDatas', $pDatas, PDO::PARAM_STR);
+            $pStmt->bindParam(':pId', $footPrint, PDO::PARAM_STR);
+            $pending = false;
 
-                    foreach($obj->entry as $entry) {
-                        if (count($entry->category) > 0) { // Insert or update tag table if tag exist
-                            $tags = '';
-                            $tQuery = "INSERT INTO tags(tag)
-                                       VALUES (:tag) ON DUPLICATE KEY UPDATE hits = hits+1;";
-                            $tStmt = dbConnexion::getInstance()->prepare($tQuery);
-                            $tStmt->bindParam(':tag', $tag, PDO::PARAM_STR);
-                            foreach($entry->category as $elements) {
-                                $tag = trim($elements['term']);
-                                //$tStmt->execute();
-                                $tags .= $elements['term'] . ' ';
-                            }
-                            $tStmt->closeCursor();
-                            $tStmt = NULL;
-                        }
-                        $sharer     = $id;
-                        $link_hash  = md5($entry->link['href']);
-                        $link       = $entry->link['href'];
-                        $title      = $entry->title;
-                        $content    = $entry->content;
-                        $taglist    = isset($tags) ? $tags : '';
-                        $permalink  = self::urlToPermalink($entry->id);
-                        $published  = $entry->published;
-                        $updated    = $entry->updated;
-                        $firstShare = '';
-                        $smallHash  = $this->smallHash(rand() . $permalink);
-                        $origin     = $this->ifReShared($entry);
-                        if (is_array($origin)) {
-                            if ($origin['shared'] === false) {
-                                $pSharer = $id;
-                                $pObject = json_encode($entry);
-                                $pHash   = md5($pObject);
-                                $pending = true;
-                                $pStmt->execute();
-                            }
-                            else {
-                                $firstShare = $origin['shared'];
-                                $stmt->execute();
-                            }
-                        }
-                        else {
-                            $stmt->execute();
-                        }
+            foreach($datas as $entry) {
+                if (is_array($entry['tags'])) { // Insert or update tag table if tag exist
+                    $tQuery = "INSERT INTO tags(tag)
+                        VALUES (:tag) ON DUPLICATE KEY UPDATE hits = hits+1;";
+                    $tStmt = dbConnexion::getInstance()->prepare($tQuery);
+                    $tStmt->bindParam(':tag', $tag, PDO::PARAM_STR);
+                    foreach($entry['tags'] as $element) {
+                        $tag = trim($element);
+                        $tStmt->execute();
                     }
-                    $stmt->closeCursor();
-                    $pStmt->closeCursor();
-                    $stmt = NULL;
+                    $tStmt->closeCursor();
+                    $tStmt = NULL;
+                }
+                $sharer     = $id;
+                $link_hash  = md5($entry['link']);
+                $link       = $entry['link'];
+                $title      = $entry['title'];
+                $content    = $entry['content'];
+                $taglist    = is_array($entry['tags']) ? implode(',', $entry['tags']) : '';
+                $permalink  = self::urlToPermalink($entry['permalink']);
+                $published  = $entry['published'];
+                $updated    = $entry['updated'];
+                $firstShare = '';
+                $footPrint  = self::footPrint($entry['permalink']);
+                $origin     = $this->ifReShared($entry);
+                if (is_array($origin)) {
+                    if ($origin['shared'] === false) {
+                        $pSharer = $id;
+                        $pDatas  = serialize($entry);
+                        $pending = true;
+                        $pStmt->execute();
+                    }
+                    else {
+                        $firstShare = $origin['shared'];
+                        $stmt->execute();
+                    }
+                }
+                else {
+                    $stmt->execute();
                 }
             }
+            $stmt->closeCursor();
+            $pStmt->closeCursor();
+            $stmt = $pStmt = NULL;
         }
         return $pending;
     }
@@ -131,9 +130,9 @@ class flowDb
      * @param  string $string [description]
      * @return string $hash   [description]
      */
-    private function smallHash($string)
+    private static function footPrint($string)
     {
-        $hash = rtrim(base64_encode(hash('crc32', $string, TRUE)), '=');
+        $hash = rtrim(base64_encode(hash('md5', $string, TRUE)), '=');
         $hash = str_replace('+', '-', $hash); // Get rid of characters which need encoding in URLs.
         $hash = str_replace('/', '_', $hash);
         $hash = str_replace('=', '@', $hash);
@@ -165,11 +164,11 @@ class flowDb
         }
     }
 
-    private function ifReShared($obj)
+    private function ifReShared($entry)
     {
-        if (is_object($obj)) {
+        if (is_array($entry)) {
             $pattern = '/via([\s:]*)?<a href="(?P<href>(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)">/i';
-            $text    = $obj->content;
+            $text    = $entry['content'];
             preg_match($pattern, $text, $matches);
             if (count($matches) > 0) {
                 $elements = parse_url($matches['href']);
@@ -177,7 +176,7 @@ class flowDb
                 $shaarli = array_shift($elements) . '://' . implode($elements);
                 if ($this->searchSharer($shaarli, true)) {
                 echo $shaarli;
-                    $shared = $this->linkNotExists($obj->link['href']);
+                    $shared = $this->linkNotExists($entry['link']);
                     if ($shared === false) {
                         $result['cause']  = 'source_not_in_database';
                         $result['shared'] = false;
