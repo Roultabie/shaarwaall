d985723824fb15fadd28c6a5d91ceaaa933b618b
Daniel Douat <daniel.douat@gorgones.net>
2018-06-06 22:46:16 +0200

    tested more complete thread

--- backend/threads.php
+++ backend/threads.php
@@ -2,16 +2,32 @@
 class getFeedUpdate extends Thread
 {
     private $sharers;
-    public function __construct($sharers)
+    public function __construct($sharers, $flowDb)
     {
-        $this->sharers = $sharers;
+        $this->sharers  = $sharers;
+        $this->flowDb   = $flowDb;
+        $this->nb      = 15;
     }
 
     public function run()
     {
         foreach ($this->sharers as $sharer) {
-            $feed = feedParser::loadFeed($sharer->uri, 1);
-            $url  = $sharer->feed;
+            $atom       = feedParser::loadFeed($sharer->uri, 1);
+            $feedUpdate = (string) $atom->updated;
+            $feedUpdate = flowDb::filterDate($feedUpdate);
+            $oldUpdate  = $this->sharer->updated;
+            $newUpdate  = strtotime($feedUpdate);
+            if ($oldUpdate < $newUpdate) {
+                $feed   = feedParser::loadFeed($sharer->uri, $this->nb);
+                $entry  = flowDb::flowToArray($feed->entry, 'ASC', 'updated');
+                $result = $this->flowDb->setFlow($sharer, $entry);
+                if (is_array($entry)) {
+                    $this->flowDb->setTags($result['tags']);
+                    $this->flowDb->setSharerLastEntryUpdated($sharer->id, $result['update']);
+                    $this->flowDb->setSharerUpdatedFeed($sharer->id, $result['update']);
+                }
+
+            }
             //var_dump($url);
         }
     }
